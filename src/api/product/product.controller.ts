48c631feda3e28a617dc4343cc0f2ff8d8efc1c2
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-11-23 01:25:56 +0530

    Implement ProductService and ProductController with CRUD operations

--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { type IProduct, ProductService } from './product.service';
 
-@Controller('product')
+@Controller('/product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Get()
-  getProduct(): string {
-    return 'Product';
+  @Get('/all')
+  allProducts(): IProduct[] {
+    return this.productService.getAllProducts();
+  }
+
+  @Get('/by-id/:id')
+  productById(@Param('id') id: number): IProduct {
+    console.log(id);
+    return this.productService.getById(id);
+  }
+
+  @Post('/create')
+  createProduct(@Body() newProductData: any) {
+    console.log(newProductData);
+    return this.productService.createProduct(newProductData);
+  }
+
+  @Put('/update/:id')
+  updateProduct(@Param('id') id: number, @Body() updatedProductData: any) {
+    return this.productService.updateProduct(id, updatedProductData);
+  }
+
+  @Put('/delete/:id')
+  deleteProduct(@Param('id') id: number) {
+    return this.productService.deleteProduct(id);
   }
 }
--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  rating: number;
+}
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private products: IProduct[] = [];
+
+  getAllProducts(): IProduct[] {
+    return this.products;
+  }
+
+  getById(id: number): IProduct {
+    const product = this.products.find(
+      (product) => product.id === parseInt(id.toString()),
+    );
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    return product;
+  }
+
+  createProduct(product: IProduct) {
+    this.products.push({ ...product, id: this.products.length + 1 });
+    return {
+      message: 'Product created successfully',
+      product: product,
+    };
+  }
+
+  updateProduct(id: number, product: IProduct) {
+    const index = this.products.findIndex(
+      (p) => p.id === parseInt(id.toString()),
+    );
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...product };
+      return { message: 'Product updated successfully' };
+    } else {
+      throw new NotFoundException('Product not found');
+    }
+  }
+
+  deleteProduct(id: number) {
+    const index = this.products.findIndex(
+      (p) => p.id === parseInt(id.toString()),
+    );
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      return { message: 'Product deleted successfully' };
+    } else {
+      throw new NotFoundException('Product not found');
+    }
+  }
+}
